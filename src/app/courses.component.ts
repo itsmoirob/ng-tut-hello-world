dcd551fa152d5141442070771cd838a2536821a6
Robbie <rob.w.sherman@gmail.com>
2017-07-20 18:19:43 +0100

    Progressed course

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -12,15 +12,22 @@ import { DomSanitizer } from '@angular/platform-browser';
 <ul>
   <li *ngFor="let course of courses">{{course}}</li>
 </ul>
+<button class="btn btn-primary" [class.btn-success]="isActive">Save</button>
+<button class="btn btn-primary" [style.backgroundColor] = "isActive ? 'green' : 'tomato'">Save</button>
+<div (click)="onDiv()">
+  <button class="btn btn-primary" (click)="onSave($event)" >Save</button>
+</div>
+<input  (keyup.enter)="onKeyUp()">
 `
 })
 
 export class CoursesComponent {
   title = 'List of courses';
-  imageUrl=' https://lorempixel.com/400/200';
+  imageUrl = ' https://lorempixel.com/400/200';
   courses: string[];
+  isActive: boolean = false;
 
-  constructor( private service: CoursesService, private sanitizer: DomSanitizer ) {
+  constructor(private service: CoursesService, private sanitizer: DomSanitizer) {
     this.courses = service.getCourses();
   }
 
@@ -28,7 +35,21 @@ export class CoursesComponent {
     return this.title;
   }
 
-  sanitizeImg () {
-     return this.sanitizer.bypassSecurityTrustUrl(this.imageUrl);
-     }
+  sanitizeImg() {
+    return this.sanitizer.bypassSecurityTrustUrl(this.imageUrl);
+  }
+
+  onSave($event) {
+    $event.stopPropagation();
+    console.log('hi there');
+    console.log($event);
+  }
+
+  onDiv() {
+    console.log('Div clicked');
+  }
+
+  onKeyUp() {
+    console.log('Enter key was pressed');
+  }
 }
